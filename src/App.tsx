70be588727fb371004dfe2341dfc95beaf13374c
Howard Yeh <howard@metacircus.com>
2016-08-07 08:59:12 +0800

    frontend stuff

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export function App() {
+	return (
+		<div>Hello World</div>
+	);
+}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { App } from "./App";
+
+import * as React from "react";
+import { render } from "react-dom";
+
+window.addEventListener("load", () => {
+	const $root = document.querySelector("#react-root");
+
+	render(<App/>, $root);
+});
