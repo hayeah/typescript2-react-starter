79a0f707523e203b713d1d3324dfefe3edbdd1dc
Howard Yeh <howard@metacircus.com>
2016-08-07 08:58:49 +0800

    random tweaks

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="globals/chai/index.d.ts" />
 /// <reference path="globals/enzyme/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
